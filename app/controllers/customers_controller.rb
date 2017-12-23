7f23c33275a497b63f15317da4e654e5d0664d05
Kaneko Keiko <keiko.cda@gmail.com>
2017-12-24 00:32:11 +0900

    Add customer resistration page

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,5 +1,15 @@
 class CustomersController < ApplicationController
   def new
+    @customer = Customer.new
+  end
+
+  def create
+    @customer = Customer.new(customer_params)
+    if @customer.save
+      render '/customers/index'
+    else
+      render '/customers/new'
+    end
   end
 
   def show
@@ -10,4 +20,9 @@ class CustomersController < ApplicationController
 
   def destroy
   end
+
+  def customer_params
+    params.require(:customer).permit(:code, :name, :address)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/' => 'top#index'
-  get "/products/new" => 'products#new'
 
-  get '/customers' => 'customers#index'
+  get '/customers/index' => 'customers#index'
   get '/customers/new' => 'customers#new'
+  post '/customers' => 'customers#create'
+
+  get '/products/index' => 'products#index'
+  get "/products/new" => 'products#new'
 end

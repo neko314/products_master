3db25c5a41a2a6362c098b1562353b15d344532a
alpaca-tc <alpaca-tc@alpaca.tc>
2017-12-29 21:48:43 +0900

    Remove unused column from products

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171224104313) do
+ActiveRecord::Schema.define(version: 20171229115839) do
 
   create_table "customers", force: :cascade do |t|
     t.string "code"
@@ -30,9 +30,6 @@ ActiveRecord::Schema.define(version: 20171224104313) do
   create_table "products", force: :cascade do |t|
     t.integer "code"
     t.string "name"
-    t.string "material"
-    t.integer "usage"
-    t.string "customer"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "customer_id"
